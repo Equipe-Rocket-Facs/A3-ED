2388bb40425e04990c7bb248b73172d9e35f82ae
Reinan Carvalho Amaral <reinanff@gmail.com>
2025-06-09 22:49:17 -0300

    finish

--- src/main/java/com/equiperocket/projects/MainAVLTreeGUI.java
+++ src/main/java/com/equiperocket/projects/MainAVLTreeGUI.java
@@ -2,11 +2,10 @@ package com.equiperocket.projects;
 
 import com.equiperocket.projects.avltree.AVLTree;
 import com.equiperocket.projects.avltree.AVLTreeVisualizerGUI;
-import com.equiperocket.projects.avltree.AVLTreeVisualizerGUIAP;
 
 public class MainAVLTreeGUI {
     public static void main(String[] args) {
         AVLTree tree = new AVLTree();
-        new AVLTreeVisualizerGUIAP(tree);
+        new AVLTreeVisualizerGUI(tree);
     }
 }
